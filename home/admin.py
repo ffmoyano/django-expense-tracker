aae685bb8464b06c94fecfdd5a649b9c7cc4f7b5
ffmoyano <f2m25582@gmail.com>
2023-02-04 21:21:42 +0100

    enhanced models

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from home.models import IncomeType, ExpenseType, Income, Expense
+from home.models import IncomeType, ExpenseType, Income, Expense, LabelColor
 
 admin.site.register(Income)
 admin.site.register(Expense)
+admin.site.register(IncomeType)
+admin.site.register(ExpenseType)
+admin.site.register(LabelColor)
--- home/models.py
+++ home/models.py
@@ -1,35 +1,40 @@
 from django import forms
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 
-class ExpenseType(models.TextChoices):
-    WORK = "Trabajo",
-    FOOD = "Comida",
+class LabelColor(models.Model):
+    background = models.CharField(max_length=6)
+    foreground = models.CharField(max_length=6)
 
 
-class IncomeType(models.TextChoices):
-    PAYROLL = "Nómina",
+class ExpenseType(models.Model):
+    type = models.CharField(max_length=50)
+    color = models.ForeignKey('LabelColor', on_delete=models.SET_NULL, null=True)
+
+
+class IncomeType(models.Model):
+    type = models.CharField(max_length=50)
+    color = models.ForeignKey('LabelColor', on_delete=models.SET_NULL, null=True)
 
 
 class Movement(models.Model):
     abstract = True
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(default=now)
     concept = models.CharField(max_length=400, default='')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Expense(Movement):
-    type = models.CharField(choices=ExpenseType.choices,
-                            max_length=50)
+    type = models.ForeignKey('ExpenseType', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'Expense: {self.id} ({self.date} {self.type} {self.concept})'
 
 
 class Income(Movement):
-    type = models.CharField(choices=IncomeType.choices,
-                            max_length=50)
+    type = models.ForeignKey('IncomeType', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'Income: {self.id} ({self.date} {self.type} {self.concept})'
